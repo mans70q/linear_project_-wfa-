39da5424b3480ec7c36fe5d466b48f2d22f58071
عبدالرحمن محمد منصور <121037018+mans70q@users.noreply.github.com>
2023-12-08 19:55:09 +0200

    Add files via upload

---
+++ linear project win/linear project win/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace linear_project_win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matrix = new System.Windows.Forms.RichTextBox();
+            this.m_num_m = new System.Windows.Forms.Button();
+            this.m_num_p = new System.Windows.Forms.Button();
+            this.n_num_m = new System.Windows.Forms.Button();
+            this.n_num_p = new System.Windows.Forms.Button();
+            this.m_num = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_size = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.n_num = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.steps = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // matrix
+            // 
+            this.matrix.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.matrix.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.matrix.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.matrix.Location = new System.Drawing.Point(234, 146);
+            this.matrix.Name = "matrix";
+            this.matrix.Size = new System.Drawing.Size(433, 190);
+            this.matrix.TabIndex = 0;
+            this.matrix.Text = "";
+            // 
+            // m_num_m
+            // 
+            this.m_num_m.Location = new System.Drawing.Point(139, 90);
+            this.m_num_m.Name = "m_num_m";
+            this.m_num_m.Size = new System.Drawing.Size(28, 23);
+            this.m_num_m.TabIndex = 1;
+            this.m_num_m.Text = "-";
+            this.m_num_m.UseVisualStyleBackColor = true;
+            this.m_num_m.Click += new System.EventHandler(this.m_num_m_Click);
+            // 
+            // m_num_p
+            // 
+            this.m_num_p.Location = new System.Drawing.Point(173, 90);
+            this.m_num_p.Name = "m_num_p";
+            this.m_num_p.Size = new System.Drawing.Size(25, 23);
+            this.m_num_p.TabIndex = 2;
+            this.m_num_p.Text = "+";
+            this.m_num_p.UseVisualStyleBackColor = true;
+            this.m_num_p.Click += new System.EventHandler(this.m_num_p_Click);
+            // 
+            // n_num_m
+            // 
+            this.n_num_m.Location = new System.Drawing.Point(394, 90);
+            this.n_num_m.Name = "n_num_m";
+            this.n_num_m.Size = new System.Drawing.Size(28, 23);
+            this.n_num_m.TabIndex = 3;
+            this.n_num_m.Text = "-";
+            this.n_num_m.UseVisualStyleBackColor = true;
+            this.n_num_m.Click += new System.EventHandler(this.n_num_m_Click);
+            // 
+            // n_num_p
+            // 
+            this.n_num_p.Location = new System.Drawing.Point(428, 90);
+            this.n_num_p.Name = "n_num_p";
+            this.n_num_p.Size = new System.Drawing.Size(25, 23);
+            this.n_num_p.TabIndex = 4;
+            this.n_num_p.Text = "++";
+            this.n_num_p.UseVisualStyleBackColor = true;
+            this.n_num_p.Click += new System.EventHandler(this.n_num_p_Click);
+            // 
+            // m_num
+            // 
+            this.m_num.AutoSize = true;
+            this.m_num.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_num.Location = new System.Drawing.Point(158, 65);
+            this.m_num.MaximumSize = new System.Drawing.Size(50, 50);
+            this.m_num.Name = "m_num";
+            this.m_num.Size = new System.Drawing.Size(23, 24);
+            this.m_num.TabIndex = 5;
+            this.m_num.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(351, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "n";
+            // 
+            // lbl_size
+            // 
+            this.lbl_size.AutoSize = true;
+            this.lbl_size.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_size.Location = new System.Drawing.Point(687, 67);
+            this.lbl_size.Name = "lbl_size";
+            this.lbl_size.Size = new System.Drawing.Size(27, 28);
+            this.lbl_size.TabIndex = 9;
+            this.lbl_size.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(720, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 34);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "size";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(89, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 21);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "m";
+            // 
+            // n_num
+            // 
+            this.n_num.AutoSize = true;
+            this.n_num.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n_num.Location = new System.Drawing.Point(412, 65);
+            this.n_num.Name = "n_num";
+            this.n_num.Size = new System.Drawing.Size(23, 24);
+            this.n_num.TabIndex = 7;
+            this.n_num.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 49);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(673, 197);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 50);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "submit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(352, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Enter The Elements Sequentially ⬇";
+            // 
+            // steps
+            // 
+            this.steps.BackColor = System.Drawing.Color.White;
+            this.steps.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.steps.Location = new System.Drawing.Point(31, 146);
+            this.steps.Name = "steps";
+            this.steps.ReadOnly = true;
+            this.steps.Size = new System.Drawing.Size(186, 381);
+            this.steps.TabIndex = 19;
+            this.steps.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(859, 539);
+            this.Controls.Add(this.steps);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl_size);
+            this.Controls.Add(this.n_num);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.m_num);
+            this.Controls.Add(this.n_num_p);
+            this.Controls.Add(this.n_num_m);
+            this.Controls.Add(this.m_num_p);
+            this.Controls.Add(this.m_num_m);
+            this.Controls.Add(this.matrix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.RichTextBox matrix;
+        public System.Windows.Forms.Button m_num_m;
+        public System.Windows.Forms.Button m_num_p;
+        public System.Windows.Forms.Button n_num_m;
+        public System.Windows.Forms.Button n_num_p;
+        public System.Windows.Forms.Label m_num;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label lbl_size;
+        public System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label label7;
+        public System.Windows.Forms.Label n_num;
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox steps;
+    }
+}
+
---
+++ linear project win/linear project win/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace linear_project_win
+{
+    public partial class Form1 : Form
+    {
+        static public int num_of_r = 0;
+        static public int num_of_c = 0;
+        static public int Msize = 0;
+        public double[,] arr;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //   →→→ جميع العمليات اللي بتحصل على المصفوفة   
+        public void Print(double[,] arr, string str)
+        { 
+            matrix.Clear();
+            matrix.AppendText(str.ToString()+"\n");
+            for (int j = 0; j < num_of_r; j++)
+            {
+                matrix.AppendText("| ");
+                for (int i = 0; i < num_of_c; i++)
+                {
+                    matrix.AppendText(arr[j, i].ToString() + " ");
+                }
+                matrix.AppendText(" |\n");
+            }
+        }
+        public void Go_Down(ref double[,] data, int row ,int print)
+        {
+
+            double[] ch = new double[data.GetLength(1)];
+
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                ch[i] = data[data.GetLength(0) - 1, i];
+            }
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                data[data.GetLength(0) - 1, i] = data[row, i];
+            }
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                data[row, i] = ch[i];
+            }Print(arr, "");
+          if(print==0 && row!=num_of_r-1)  steps.AppendText($"R{row+1}-> R{num_of_r} \n");
+        }
+        public void Swap(ref double[,] data, int row)
+        {
+            double[] ch = new double[data.GetLength(1)];
+
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                ch[i] = data[0, i];
+            }
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                data[0, i] = data[row, i];
+            }
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                data[row, i] = ch[i];
+            }
+            steps.AppendText($"swapping R1->R{row+1}\n");
+        }
+        public void ToLeading(ref double[,] data)
+        {
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                if (data[0, j] == 1) goto End;
+                if (data[0, j] != 0 && data[0, j] != 1)
+                {
+                    double dev = data[0, j];
+                    steps.AppendText($"R / {dev} -> R\n");
+                    for (int i = 0; i < data.GetLength(1); ++i)
+                    {
+                        data[0, i] /= dev;
+                    }
+                    Print(data, "to leading");
+                    goto End;
+                }
+            }
+        End:;
+        }
+        public  void To_Row_Echlon(ref double[,] data, int col)
+        {
+            double num = 0;
+            double[] arr = new double[data.GetLength(1)];
+            for (int i = 1; i < data.GetLength(0); i++)
+            {
+                for (int j = col; j < data.GetLength(1); j++)
+                {
+                    if (j == col && data[i, j] != 0)
+                    {num = -data[i, j];
+                        steps.AppendText($"R{i} *({num}) +R{i + 1}->R{i+1}\n");
+                        for (int k = 0; k < data.GetLength(1); k++)
+                            arr[k] = data[0, k] * num;
+
+                        for (int l = 0; l < data.GetLength(1); l++)
+                            data[i, l] += arr[l];
+                    }
+                }
+            }
+            
+            Print(data, "to zero");
+        }
+        public void To_Reduced_Row_Echlon(ref double[,] data, int leadi, int leadj, int chi)
+        {
+            steps.AppendText($"R{leadi + 1}*-{data[chi,leadj]} +R{chi+1} ->R{chi + 1}");
+            double[] change = new double[data.GetLength(1)];
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                change[i] = data[leadi, i];
+            }
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                change[i] = change[i] * -1 * data[chi, leadj];
+            }
+            for (int i = 0; i < data.GetLength(1); i++)
+            {
+                data[chi, i] += change[i];
+            }
+            Print(data, "Reduced row_echlon form");
+        }
+
+
+        // التحكم في عدد الصفوف والاعمدة
+        public void m_num_p_Click(object sender, EventArgs e)
+        {
+            num_of_r++;
+            m_num.Text = num_of_r.ToString();
+            Msize = num_of_r * num_of_c;
+            lbl_size.Text = Msize.ToString();
+        }
+
+        public void m_num_m_Click(object sender, EventArgs e)
+        {
+            num_of_r--;
+            m_num.Text = num_of_r.ToString();
+            Msize = num_of_r * num_of_c;
+            lbl_size.Text = Msize.ToString();
+        }
+
+        public void n_num_p_Click(object sender, EventArgs e)
+        {
+            num_of_c++;
+            n_num.Text = num_of_c.ToString();
+            Msize = num_of_r * num_of_c;
+            lbl_size.Text = Msize.ToString();
+        }
+
+        public void n_num_m_Click(object sender, EventArgs e)
+        {
+            num_of_c--;
+            n_num.Text = num_of_c.ToString();
+            Msize = num_of_r * num_of_c;
+            lbl_size.Text = Msize.ToString();
+        }
+
+        // قراية المصفوفة من اليوزر
+        public void button6_Click(object sender, EventArgs e)
+        {
+            string a = matrix.Text;
+            steps.Clear();
+            int count = 0;
+            string[] y = a.Split(' ');
+            arr = new double[num_of_r, num_of_c];
+            for (int j = 0; j < num_of_r; j++)
+            {
+                for (int i = 0; i < num_of_c; i++)
+                {
+                    arr[j, i] = double.Parse(y[count++]);
+                }
+            }
+            Print(arr,"your matrix is :");
+        }
+        
+        //  ♥♥♥♥ الكود الرئيسي  
+        public void button1_Click(object sender, EventArgs e)
+        {
+            double[,] fArr = new double[num_of_r, num_of_c];
+            for (int start = 0; start < num_of_r; start++)
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    int x = 0;
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[i, j] != 0)
+                        {
+                            x = 1;
+                        }
+                    }
+                    if (x == 0) Go_Down(ref arr, i ,start);
+                }
+
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[0, j] == 0 && arr[i, j] != 0)
+                        {
+                            Swap(ref arr, i);
+                            goto end_of_the_loop;
+                        }
+
+                    }
+
+                }
+            end_of_the_loop:
+                Print(arr, "after swapping");
+
+
+                ToLeading(ref arr);
+                // جعل باقي عناصر العامود =0
+
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        if (arr[i, j] == 1)
+                        {
+                            To_Row_Echlon(ref arr, j);
+                            goto end_of_the_loop2;
+                        }
+                    }
+                }
+            end_of_the_loop2:
+                //تخزين الصف الاول في مصفوفه جديده وحذفه
+                
+                for (int j = 0; j < num_of_c; j++)
+                {
+                    fArr[start, j] = arr[0, j];
+                    arr[0, j] = 0;
+                }
+            }
+            Print(fArr, "the row-echlon form");
+            //تحويل المصفوفه الى ريديوسد رو ايشلون فورم 
+            for (int i = 0; i < num_of_r; i++)
+            {
+                for (int j = 0; j < num_of_c - 1; j++)
+                {
+                    int X;
+                    if (fArr[i, j] == 1)
+                    {
+                        X = i;
+                        for (int k = 0; k < num_of_r; k++)
+                        {
+                            if (fArr[k, j] != 0 && k != X) To_Reduced_Row_Echlon(ref fArr, i, j, k);
+                        }
+                        break;
+                    }
+                }
+            }
+            //السيستم ليه اكتر من حل
+            int flag = 0;
+            for (int i = 0; i < num_of_r; i++)
+            {
+                flag = 0;
+                for (int j = 0; j < num_of_c ; j++)
+                {
+                    if (fArr[i, j] == 0) { flag++; }
+                    if (flag == arr.GetLength(1)) { MessageBox.Show("the linear system has infinitely many solutions"); goto end; }
+                }
+            }
+            //السيستم الله الوكيل مش إله حل
+            int flag2 = 0;
+            for (int i = 0; i < num_of_r; ++i)
+            {flag2 = 0;
+                for (int j = 0; j < num_of_c; j++)
+                {
+                    if (fArr[i, j] == 0) { flag2++; }
+                    if (arr.GetLength(1)-1 == flag2 && fArr[i, num_of_c-1] != 0)
+                    {
+                        matrix.AppendText("system has no solutions"); goto end;
+                    }
+                }
+            }
+
+            //الناتج النهائي للمتغيرات كلها
+            for (int i = 0; i < num_of_r; i++)
+            {
+                for (int j = 0; j < num_of_c; j++)
+                {
+                    if (fArr[i, j] == 1)
+                    {
+                        matrix.AppendText($"x{j + 1} = {fArr[i, num_of_c - 1]}\n"); break;
+                    }
+                }
+            }
+        end:;
+        }
+    }
+}
